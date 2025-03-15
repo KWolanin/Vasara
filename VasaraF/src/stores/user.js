671b1f5d873d07ce3dfba126e8cffc0440049130
Katarzyna Wolanin <wolanin4u@gmail.com>
2025-03-15 22:05:13 +0100

    Pinia - user store rewritten to composition api

--- VasaraF/src/stores/user.js
+++ VasaraF/src/stores/user.js
@@ -1,45 +1,82 @@
 import { defineStore } from "pinia";
+import { ref } from "vue";
 
-export const useUserStore = defineStore("user", {
-  state: () => ({
-    id: null,
-    username: null,
-    login: null,
-    email: null
-  }),
-  actions: {
-    saveUser(id, username, login, email) {
-      this.id = id;
-      this.username = username;
-      this.login = login;
-      this.email = email;
-      const user = { id, username, login, email };
-      localStorage.setItem("user", JSON.stringify(user));
-    },
-    loadUser() {
-      const user = JSON.parse(localStorage.getItem("user"));
-      if (user) {
-        this.id = user.id;
-        this.username = user.username;
-        this.login = user.login;
-        this.email = user.email;
-      }
-    },
-    logout() {
-      this.id = null;
-      this.username = null;
-      this.login = null;
-      this.email = null;
-      localStorage.removeItem("user");
-      localStorage.removeItem("token");
-    },
-    updateEmail(email) {
-      this.email = email;
-      localStorage.setItem("user", JSON.stringify(this.$state));
-    },
-    updateUsername(username) {
-      this.username = username;
-      localStorage.setItem("user", JSON.stringify(this.$state));
+export const useUserStore = defineStore("user", () => {
+  const id = ref(null);
+  const username = ref(null);
+  const login = ref(null);
+  const email = ref(null);
+
+  const saveUser = (inputId, inputUsername, inputLogin, inputEmail) => {
+    id.value = inputId;
+    username.value = inputUsername;
+    login.value = inputLogin;
+    email.value = inputEmail;
+    localStorage.setItem(
+      "user",
+      JSON.stringify({
+        id: inputId,
+        username: inputUsername,
+        login: inputLogin,
+        email: inputEmail,
+      })
+    );
+  };
+
+  const loadUser = () => {
+    const user = JSON.parse(localStorage.getItem("user"));
+    if (user) {
+      id.value = user.id;
+      username.value = user.username;
+      login.value = user.login;
+      email.value = user.email;
     }
-  },
+  };
+
+  const logout = () => {
+    id.value = null;
+    username.value = null;
+    login.value = null;
+    email.value = null;
+    localStorage.removeItem("user");
+    localStorage.removeItem("token");
+  };
+
+  const updateEmail = (emailInput) => {
+    email.value = emailInput;
+    localStorage.setItem(
+      "user",
+      JSON.stringify({
+        id: id.value,
+        username: username.value,
+        login: login.value,
+        email: email.value,
+      })
+    );
+  };
+
+  const updateUsername = (usernameInput) => {
+    username.value = usernameInput;
+    localStorage.setItem(
+      "user",
+      JSON.stringify({
+        id: id.value,
+        username: username.value,
+        login: login.value,
+        email: email.value,
+      })
+    );
+  };
+
+  return {
+    id,
+    username,
+    login,
+    email,
+    saveUser,
+    loadUser,
+    logout,
+    updateEmail,
+    updateUsername,
+  };
 });
