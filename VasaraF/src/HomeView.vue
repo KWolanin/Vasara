6986a9ddd4fcf9b41e467640f9ad6e30c37103e4
Katarzyna Wolanin <wolanin4u@gmail.com>
2025-03-19 14:58:30 +0100

    Fixed: responsibility for home page

--- VasaraF/src/HomeView.vue
+++ VasaraF/src/HomeView.vue
@@ -5,19 +5,24 @@
         <div class="text-center">
           <h1 class="title">Vasara</h1>
           <h3 class="subtitle">your summer trip to stories</h3>
-          <div>
-            <img src="./assets/favicon.ico" width="150" height="150" />
+          <img
+            src="./assets/favicon.ico"
+            class="responsive-img"
+            width="150"
+            height="150"
+          />
+          <div class="row justify-center q-mt-md">
+            <q-btn class="q-ma-md" flat v-if="isLoggedIn">
+              <RouterLink to="create">
+                <h2 class="btn">Create</h2>
+              </RouterLink>
+            </q-btn>
+            <q-btn class="q-ma-md" flat>
+              <RouterLink to="readAll">
+                <h2 class="btn">Read</h2>
+              </RouterLink>
+            </q-btn>
           </div>
-          <q-btn class="q-ma-md" flat>
-            <RouterLink to="create" v-if="isLoggedIn">
-              <h2 class="btn">Create</h2>
-            </RouterLink>
-          </q-btn>
-          <q-btn class="q-ma-md" flat>
-            <RouterLink to="readAll">
-              <h2 class="btn">Read</h2>
-            </RouterLink>
-          </q-btn>
         </div>
       </q-page>
     </q-page-container>
@@ -58,5 +63,42 @@ const isLoggedIn = computed(() => !!userStore.id);
   font-weight: 400;
   font-style: normal;
   color: #333;
+}
+
+.responsive-img {
+  max-width: 100%;
+  height: auto;
+}
+
+@media (max-width: 768px) {
+  .title {
+    font-size: 6rem;
+  }
+
+  .subtitle {
+    font-size: 1.2rem;
+  }
+
+  .btn {
+    font-size: 2rem;
+  }
+}
+
+@media (max-width: 480px) {
+  .title {
+    font-size: 4rem;
+  }
+
+  .subtitle {
+    font-size: 1rem;
+  }
+
+  .btn {
+    font-size: 1.5rem;
+  }
+
+  .q-btn {
+    width: 100%;
+  }
 }
 </style>
